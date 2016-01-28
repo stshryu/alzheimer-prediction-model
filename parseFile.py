a5ed87718fbec8fb998ce44833e91cb5d9898427
shstyoo <shstyoo@gmail.com>
2016-01-28 03:08:33 -0500

    finished file parser. output.txt contains important metadata from .soft files

--- parseFile.py
+++ parseFile.py
@@ -5,6 +5,16 @@ for (dirpath, dirnames, filenames) in walk("GEO_studies"):
     f.extend(filenames)
     break
 
+var = ["!dataset_title =", "!dataset_sample_count =", "!dataset_update_date =", "!dataset_platform_organism =", "!dataset_pubmed_id =", "!dataset_platform =", "!dataset_platform_technology_type =", "!dataset_reference_series =", "!dataset_description ="]
+var_val = []
+
+f_Out = open("output.txt", 'w')
+
 for item in f:
-    _file = open(item, "r")
-    print(_file.readline)
+    _file = open("GEO_studies/" + item, "r")
+    f_Out.write("++++++++####### NEW FILE #######++++++++\n")
+    for lines in _file:
+        for items in var:
+            if items in lines:
+                print(lines.strip())
+                f_Out.write(lines)
